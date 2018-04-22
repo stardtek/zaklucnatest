a187ee19604b2efca7f5e017675caa6c1909d5cb
rokserak <rok.serak@gmail.com>
2018-04-22 17:16:58 +0200

    meni togler dela

--- public/javascript/angularKoda.js
+++
@@ -1,7 +0,0 @@
-    var app = angular.module("app", []);
-    app.controller("demoController", function($scope, $http){
-        $http.get("http://127.0.0.1:3000/getNaloge").then(function(data) {
-            console.log('dela');
-            $scope.naloge = data.data;
-        });
-    });
