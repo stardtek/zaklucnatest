a7d5283e2159920b1848896ddcbeef87c697b225
rokserak <rok.serak@gmail.com>
2018-04-23 19:21:06 +0200

    dodana navodila za vnos nalog

--- my_script.py
+++ my_script.py
@@ -1,7 +1,16 @@
-def izbrisSAM(niz):
-    nov = ""
-    for i in range(len(niz)):
-        if(niz[i] not in "aeiuo"):
-            nov += niz[i]
-    print(nov)
-izbrisSAM("dela")
+class Employee:
+   'Common base class for all employees'
+   empCount = 0
+
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print(self.name)
+
+def a(emp1):
+    emp1.displayCount()
+
+a(Employee("Zara", 2000))
